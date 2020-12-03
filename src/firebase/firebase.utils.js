44c58258d1354b9c43fbbad5c6848208adfe9cb4
gentjanferati <51865179+gentjanferati@users.noreply.github.com>
2020-12-03 02:28:15 +0100

    implemented firebase utils

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,14 +11,40 @@ const config = {
     messagingSenderId: "393156418562",
     appId: "1:393156418562:web:a46f603f78c63870fddb1b",
     measurementId: "G-EK5D7NM2VX"
-  }
-
+  };
   firebase.initializeApp(config);
 
-  export const auth= firebase.auth();
-  export const firestore= firebase.firestore();
-
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+  
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+  
+    const snapShot = await userRef.get();
+  
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        });
+      } catch (error) {
+        console.log('error creating user', error.message);
+      }
+    }
+  
+    return userRef;
+  };
+  
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+  
   const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({prompt: 'select_account'});
+  provider.setCustomParameters({ prompt: 'select_account' });
   export const signInWithGoogle = () => auth.signInWithPopup(provider);
+  
   export default firebase;
+  
